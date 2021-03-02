4e41684aa8aebcf885f99102e40827dbd39ca7d0
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-02 00:57:27 -0600

    Create pila.h

---
+++ pila.h
@@ -0,0 +1,19 @@
+#ifndef PILA_H
+#define PILA_H
+
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+// Se implementará una estructura de datos Pila utilizando la clase
+// Lista hecha en la tarea 1, la cual se heredará
+// como heredar de clase a struct?
+
+struct pila : public Lista
+{
+
+};
+
+
+#endif // PILA_H
