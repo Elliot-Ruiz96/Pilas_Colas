d68192c47b1c9783398ba9517c35b7e108dc6bf1
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-01 23:35:33 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     int dim;
     int pos;
 
-    cout << "Lista de canciones al inicio. \n" << endl;
+    cout << "Lista de canciones al inicio." << endl;
     lista.imprime();
 
     cout << "Ingrese la dimension de la lista: " << endl;
@@ -24,8 +24,9 @@ int main()
     cout << "\n2. Insertar en: \n" << endl;
     cin >> pos;
     lista.insertar_en(pos);
+    lista.imprime();
 
-    cout << "\n3. Tomar: \n" << endl; //Pendiente
+    cout << "\n3. Tomar: \n" << endl;
     cin >> pos;
     lista.tomar(pos);
 
@@ -36,21 +37,26 @@ int main()
     cout << "\n5. Eliminar: \n" << endl;
     cin >> pos;
     lista.eliminar(pos);
+    lista.imprime();
 
     cout << "\n6. Mostar todo: \n" << endl;
     lista.imprime();
 
     cout << "\n7. Modificar: \n" << endl; //Pendiente
-    lista.modificar();
+    cin >> pos;
+    lista.modificar(pos);
+    lista.imprime();
 
     cout << "\n8. Vacio: \n" << endl;
     lista.vacio();
+    lista.imprime();
 
     cout << "\n9. Purga: \n" << endl;
     lista.purgar();
 
     cout << "\n10. Tamanio: \n" << endl;
     lista.tamanio();
+    lista.imprime();
 
     cout << "\n11. Tomar primero: \n" << endl; //Pendiente
     lista.tomar1();
