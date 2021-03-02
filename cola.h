9df31e8b113fcd480ba604dedd38701fead5b84b
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-02 00:57:22 -0600

    Create cola.h

---
+++ cola.h
@@ -0,0 +1,19 @@
+#ifndef COLA_H
+#define COLA_H
+
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+// Se implementará una estructura de datos Pila utilizando la clase
+// Lista hecha en la tarea 1, la cual se heredará
+// como heredar de clase a struct?
+
+struct cola : public Lista
+{
+
+};
+
+
+#endif // COLA_H
