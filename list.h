1e3bb763978a05cb09319194c832e47ba8647bb1
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-01 22:22:39 -0600

    Create list.h

---
+++ list.h
@@ -0,0 +1,38 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "node.h"
+#include "node.cpp"
+
+using namespace std;
+
+template <class T>
+
+class Lista
+{
+public:
+    Lista();
+    ~Lista();
+    void insertar_inicio(T);
+    void insertar_en(int);
+    void tomar(int);
+    void buscar(T);
+    void eliminar(int);
+    void modificar();
+    void vacio();
+    void purgar();
+    void tamanio();
+    void tomar1();
+    void imprime();
+    void teclado(int);
+    void teclado2(int);
+    void insertar_final(T);
+private:
+    Nodo<T> *m_inicio;
+    int m_num_nodos;
+};
+
+#endif // LIST_H
