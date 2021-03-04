a2b26a3676a9d2027a612a60ddc4dfd29fe6d30f
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-03 22:21:05 -0600

    Update cola.cpp

--- cola.cpp
+++ cola.cpp
@@ -2,64 +2,72 @@
 #include <conio.h>
 #include <stdlib.h>
 
-//#include "list.h"
-
 using namespace std;
 
 struct Nodo
 {
-    int dato;
+    string dato;
     // Puntero de tipo nodo ya que apunta hacia el siguiente nodo
     Nodo *siguiente;
 };
 
-void encolar(Nodo *&, Nodo *&, int);
+void encolar(Nodo *&, Nodo *&, string);
 bool cola_vacia(Nodo *);
-void desencolar(Nodo *&, Nodo *&, int &);
-void cimaC(Nodo *, int);
+void desencolar(Nodo *&, Nodo *&, string &);
+void cimaC(Nodo *, string);
+
 
 int main()
 {
     Nodo *frente = NULL;
     Nodo *fin = NULL;
-    int dato;
-    char rpt;
+    string nombre;
+    int opcion;
+
+    cout << "\nBienvenido a atencion a cliente, por favor, elija una opcion:" << endl;
+    cout << "\n1. Ingresar un nombre." << endl;
+    cout << "\n2. Sacar a la persona del inicio." << endl;
+    cout << "\n3. Mostrar el nombre la persona al inicio:" << endl;
+    cout << "\n4. Salir:" << endl;
 
     do
     {
-        cout << "\nDigite un numero: " << endl;
-        cin >> dato;
-        encolar(frente, fin, dato);
-
-        cimaC(frente, dato);
-
-        cout << "\nDesea agregar otro elemento a Pila (s/n)? " << endl;
-        cin >> rpt;
-    }
-    while((rpt == 'S')||(rpt == 's'));
-
-    cout <<"\nSacando elementos de la cola: " << endl;
-
-    while (frente != NULL)
-    {
-        desencolar(frente, fin, dato);
-        if(frente != NULL)
+        cout << "\nIngrese su opcion: " << endl;
+        cin >> opcion;
+        switch(opcion)
         {
-            cout << dato << ", ";
+        case 1:
+            cout << "\nPor favor, diga su nombre y lo pondremos en cola de espera. " << endl;
+            cin >> nombre;
+            encolar(frente, fin, nombre);
+            break;
+        case 2:
+            desencolar(frente, fin, nombre);
+            if(frente != NULL)
+            {
+                cout <<"\nSacando persona de la cola: " << endl;
+                cout << nombre << "\n ";
+            }
+            else
+            {
+                cout <<"\nSacando a la ultima persona de la cola: " << endl;
+                cout << nombre << "\n" << endl;
+            }
+            break;
+        case 3:
+            cimaC(frente, nombre);
+            break;
+        default:
+            cout << "Opcion incorrecta. " << endl;
         }
-        else
-        {
-            cout << dato << "." << endl;
-        }
-        cimaC(frente, dato);
     }
-
+    while(opcion!=4);
+    cout << "Adios!" << endl;
     getch();
-    return 0;
 }
 
 // Para insertar elementos
-void encolar(Nodo *&frente, Nodo *&fin, int n)
+void encolar(Nodo *&frente, Nodo *&fin, string n)
 {
     // 1 Crear espacio en memoria para el nodo
     Nodo *nuevo_nodo = new Nodo();
@@ -69,6 +77,7 @@ void encolar(Nodo *&frente, Nodo *&fin, int n)
     // 3 Asignar los punteros frente y fin hacia el nuevo nodo
     if(cola_vacia(frente))
     {
+        cout <<"\nNo hay personas en la cola de espera: " << endl;
         frente = nuevo_nodo;
     }
     else
@@ -76,26 +85,17 @@ void encolar(Nodo *&frente, Nodo *&fin, int n)
         fin -> siguiente = nuevo_nodo;
     }
     fin = nuevo_nodo;
-    cout << "\n\tElemento " << n << " agregado a COLA corretamente." << endl;
+    cout << "\n\t" << n << " llegando a la cola." << endl;
 }
 
 // Funcion para determinar si la cola esta vacia o no
 bool cola_vacia(Nodo *frente)
 {
     return (frente == NULL)? true : false;
-
-//    if(frente == NULL)
-//    {
-//        return true;
-//    }
-//    else
-//    {
-//        return false;
-//    }
 }
 
 // Para quitar elementos
-void desencolar(Nodo *&frente, Nodo *&fin, int &n)
+void desencolar(Nodo *&frente, Nodo *&fin, string &n)
 {
     // 1 Obtener el valor del nodo
     n = frente->dato;
@@ -114,9 +114,16 @@ void desencolar(Nodo *&frente, Nodo *&fin, int &n)
     delete aux;
 }
 
-void cimaC(Nodo *frente, int n)
+void cimaC(Nodo *frente, string n)
 {
     Nodo *muestraC = frente;
     n = muestraC->dato;
-    cout << "\nDato en la cima: " << n << "\n" << endl;
+    if(cola_vacia(frente))
+    {
+        cout <<"\nNo hay personas en la cola de espera: " << endl;
+    }
+    else
+    {
+        cout << "\nPersona al inicio de la cola: " << n << "\n" << endl;
+    }
 }
