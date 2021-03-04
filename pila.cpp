23e1d84e2bce71c715c8af7f0418e15ecc024d90
Elliot-Ruiz96 <78750648+Elliot-Ruiz96@users.noreply.github.com>
2021-03-04 01:26:30 -0600

    Update pila.cpp

--- pila.cpp
+++ pila.cpp
@@ -6,23 +6,26 @@ using namespace std;
 
 struct Nodo
 {
-    char dato;
+    char dato, dato1;
     // Puntero de tipo nodo ya que apunta hacia el siguiente nodo
     Nodo *siguiente;
-    Nodo *siguiente2;
+    Nodo *siguiente1;
 };
 
 void apilar(Nodo *&, char);
 void apilar1(Nodo *&, char);
 void desapilar(Nodo *&, char &);
+void desapilar1(Nodo *&, char &);
 void cimaP(Nodo *, char);
+void cimaP1(Nodo *, char);
 void validacion(Nodo *&, char &, Nodo *&, char &);
 
+// int pila
 int main()
 {
     Nodo *pila = NULL;
     Nodo *pila1 = NULL;
-    char dato;
+    char dato, dato1;
     int num, num_nodos;
 
     cout << "\nBienvenido al analizador de sintaxis" << endl;
@@ -31,14 +34,17 @@ int main()
 
     if(num % 2 == 0)
     {
-        cout << "Digite un caracter([] () {}): " << endl;
         for (num_nodos = 1; num_nodos <= num ; num_nodos++)
         {
+            cout << "Digite un caracter([] () {}): " << endl;
             cin >> dato;
             apilar(pila, dato);
+            cimaP(pila, dato);
             if(num_nodos > num/2)
             {
-                 apilar1(pila1, dato);
+                dato1 = dato;
+                apilar1(pila1, dato1);
+                cimaP1(pila1, dato1);
             }
         }
     }
@@ -48,10 +54,10 @@ int main()
     }
 
     cout << "\nValidacion de la apertura y cierre de caracteres: " << endl;
-    while(pila!= NULL && pila1!= NULL)
-    {
-        validacion(pila, dato, pila1, dato);
-    }
+//    while(pila != NULL)
+//    {
+//        validacion(pila, dato, pila1, dato1);
+//    }
 
     // Mientras que no sea el final de la pila
     cout <<"\nElminando todos los caracteres desde la cima: " << endl;
@@ -65,6 +71,18 @@ int main()
         else
         {
             cout << dato << "." << endl;
+        }
+    }
+    while (pila1 != NULL)
+    {
+        desapilar1(pila1, dato1);
+        if(pila1 != NULL)
+        {
+            cout << dato1 << ", ";
+        }
+        else
+        {
+            cout << dato1 << "." << endl;
         }
     }
 
@@ -89,8 +107,8 @@ void apilar(Nodo *&pila, char n)
 void apilar1(Nodo *&pila1, char n1)
 {
     Nodo *nuevo_nodo1 = new Nodo();
-    nuevo_nodo1->dato = n1;
-    nuevo_nodo1->siguiente2 = pila1;
+    nuevo_nodo1->dato1 = n1;
+    nuevo_nodo1->siguiente1 = pila1;
     pila1 = nuevo_nodo1;
 }
 
@@ -108,11 +126,30 @@ void desapilar(Nodo *&pila, char &n)
     delete aux;
 }
 
+void desapilar1(Nodo *&pila1, char &n1)
+{
+    // 1 Crear una variable *aux de tipo Nodo
+    Nodo *aux1 = pila1;
+    // 2 Igualar el n a aux señalando a dato
+    n1 = aux1->dato1;
+    // 3 Pasar pila a siguiente nodo
+    pila1 = aux1->siguiente1;
+    // 4 Eliminar aux
+    delete aux1;
+}
+
 void cimaP(Nodo *pila, char n)
 {
     Nodo *muestraC = pila;
     n = muestraC->dato;
     cout << "\nCaracter en la cima: " << n << "\n" << endl;
+}
+
+void cimaP1(Nodo *pila1, char n1)
+{
+    Nodo *muestraC1 = pila1;
+    n1 = muestraC1->dato1;
+    cout << "\nCaracter en la cima: " << n1 << "\n" << endl;
 }
 
 void validacion(Nodo *&pila, char &n, Nodo *&pila1, char &n1)
@@ -122,26 +159,26 @@ void validacion(Nodo *&pila, char &n, Nodo *&pila1, char &n1)
     pila = aux->siguiente;
 
     Nodo *aux1 = pila1;
-    n1 = aux1->dato;
-    pila1 = aux1->siguiente2;
+    n1 = aux1->dato1;
+    pila1 = aux1->siguiente1;
 
     cout << n << n1 << endl;
 
-    if (!(n == '(' && n1 == ')'))
-    {
-        cout << "Es correcto." << endl;
-    }
-    if (!(n == '[' && n1 == ']'))
-    {
-        cout << "Es correcto." << endl;
-    }
-    if (!(n == '{' && n1 == '}'))
-    {
-        cout << "Es correcto." << endl;
-    }
-    else
-    {
-        cout << "Es incorrecto." << endl;
-    }
+//    if (!(n == ( && n1 == ) ) )
+//    {
+//        cout << "Es correcto." << endl;
+//    }
+//    if (!(n == [ && n1 == ] ))
+//    {
+//        cout << "Es correcto." << endl;
+//    }
+//    if (!(n == '{' && n1 == '}'))
+//    {
+//        cout << "Es correcto." << endl;
+//    }
+//    else
+//    {
+//        cout << "Es incorrecto." << endl;
+//    }
 
 }
